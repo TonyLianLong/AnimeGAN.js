5a173472d5405d36eed27a281ab05044ab1afb9a
Tony Lian <1040424979@qq.com>
2020-05-27 14:01:41 -0700

    Updates

---
+++ precache-manifest.e29394cfecce452a1a9be200e544d39a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b2bfeae29a4fd4c58c11308f95f19b",
+    "url": "/AnimeGAN.js/index.html"
+  },
+  {
+    "revision": "f4137cdbeddf39dd0844",
+    "url": "/AnimeGAN.js/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "188298337930d0a591c6",
+    "url": "/AnimeGAN.js/static/css/main.a2ee3e59.chunk.css"
+  },
+  {
+    "revision": "f4137cdbeddf39dd0844",
+    "url": "/AnimeGAN.js/static/js/2.d980e1cb.chunk.js"
+  },
+  {
+    "revision": "5e2252a167034352fa7bed0a873eaa75",
+    "url": "/AnimeGAN.js/static/js/2.d980e1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188298337930d0a591c6",
+    "url": "/AnimeGAN.js/static/js/main.16bf3256.chunk.js"
+  },
+  {
+    "revision": "6c8b4288eb63b9934796",
+    "url": "/AnimeGAN.js/static/js/runtime-main.131defe3.js"
+  }
+]);
